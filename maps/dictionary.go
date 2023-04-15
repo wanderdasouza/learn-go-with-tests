34531cb6a1fe5ce544a87917fa114ebd44ac6a2a
Wander Souza <wanderdasouza@gmail.com>
2023-04-15 20:15:20 -0300

    Implementa CRUD em um dicionario usando map

---
+++ maps/dictionary.go
@@ -0,0 +1,61 @@
+package maps
+
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+const (
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("The world already exists in the dictionary")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = newDefinition
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	default:
+		return err
+	}
+
+	return nil
+
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
