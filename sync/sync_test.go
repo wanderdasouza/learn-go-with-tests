94559d71e03b4f6142483b2277a21b15026b91e8
Wander Souza <wanderdasouza@gmail.com>
2023-05-01 16:10:16 -0300

    Corrige NewCounter

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter.NewCounter()
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
